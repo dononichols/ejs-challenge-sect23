943a9bffc1cc15cc580190b47a90a724c9757065
Meraki_DNA <dononichols@gmail.com>
2022-12-14 11:01:03 -0500

    complete challenge7- add compose page & route

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+
+// send to compose route - not passing data
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
 // ========
 
 
